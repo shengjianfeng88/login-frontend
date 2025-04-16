77a9ac45754ba603c3097882e779b9cd79a3060b
Kai Lei <kaikeng2000@hotmail.com>
2025-04-15 23:29:16 -0700

    Removed React

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axiosInstance from "@/utils/axiosInstance"
 
