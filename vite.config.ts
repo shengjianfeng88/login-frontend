5d9b149854f828e7ce2266b702a835815ee44cc4
Stephanie Tjiong <stephanielaurentjiong@gmail.com>
2025-05-01 15:35:12 +0800

    pull the latest from main

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react(), tsconfigPaths()],
   server: {
     // 正确的 history fallback 写法
+
     fs: {
       strict: true,
     },
   },
   // history fallback 放在 build 或 preview 阶段配置
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-})
+});
