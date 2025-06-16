9ff1f2bd575a53442f918a63fff5aaf12463890f
shengjianfeng <shengjianfeng@kuaishou.com>
2025-06-16 10:02:51 +0800

    feat: auto-test

--- src/api/tryon.ts
+++ src/api/tryon.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 // 创建 axios 实例
 const axiosInstance = axios.create({
     baseURL: 'https://tryon-advanced-canary.faishion.ai',
-    headers: {
-        'Content-Type': 'application/json'
-    },
     withCredentials: true
 });
 
