ad57a7b9ab4bf2a681de7f10a0bed5cbbe574e97
shengjianfeng <shengjianfeng@kuaishou.com>
2025-10-31 19:23:55 +0800

    feat: 更新 Done 页面的跳转链接和按钮文本

--- src/pages/Done.tsx
+++ src/pages/Done.tsx
@@ -6,12 +6,12 @@ const Done = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-6">
       <div className="text-6xl text-green-500 flex items-center justify-center border-[5px] border-green-500 rounded-full p-4">
-        <MdOutlineDone size={100}/>
+        <MdOutlineDone size={100} />
       </div>
       <p className="text-2xl font-bold">You're logged in</p>
       <p className="text-base text-gray-500">You're now logged into Faishion</p>
-      <Link to="https://faishion.ai" className="w-[180px] py-2.5 mt-2 btn-primary rounded-md text-black">
-        <p className="text-center">Done</p>
+      <Link to="https://www.faishion.ai/howitworks" className="w-[180px] py-2.5 mt-2 btn-primary rounded-md text-black">
+        <p className="text-center">How It Works</p>
       </Link>
     </div>
   )
